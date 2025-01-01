49af63082f5935b59b047d874af6bc64e5ead3ac
Sheritz Sogbesan <ssogbesan@gmail.com>
2025-01-01 16:46:50 -0500

    add is_palindrome

---
+++ algoexpert/strings/easy/is_palindrome.py
@@ -0,0 +1,11 @@
+def isPalindrome(string):
+    left_idx = 0 
+    right_idx = len(string) - 1
+
+    while left_idx <= right_idx:
+        if string[left_idx] != string[right_idx]:
+            return False
+        left_idx += 1
+        right_idx -= 1
+
+    return True
